5a585fb50efa3753aa4a192c7f8f1001c1c8a8af
Gabriel Gonçalves Cunha <goncalves.cunhaa@gmail.com>
2022-05-09 15:05:23 -0300

    componentization posts, cards and home

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,11 @@
+import './styles.css';
+
 export const PostCard = ({id, cover, title, body}) => (
 
         <div className='post'>
               <img src={cover} alt={title} />
               <div key={id} className='post-content'>
-                <h1>{title}</h1>
+                <h2>{title}</h2>
                 <p>{body}</p>
               </div>
             </div>
